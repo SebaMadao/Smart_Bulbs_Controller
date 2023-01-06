e2d9c5e59929a4bf0aee82fff0b2e81b361eb19a
Fronc <sebastian.fronc.code@gmail.com>
2023-01-06 18:33:10 +0100

    add set option

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -5,7 +5,8 @@ import GUI_events
 def make_window():
     theme = 'DarkGrey15'
     sg.theme(theme)
-    space_for_layout_online_bulbs_right = 10
+    space_for_layout_online_bulbs_right = 15
+    space_for_layout_online_bulbs_set_properties = 12
 
     def set_name_with_space(name, name_space):
         spaces = name_space - len(name) - 2
@@ -24,6 +25,8 @@ def make_window():
                                   sg.Text("NO DATA", key='-TEXT-POWER-STATUS-')],
                                  [set_name_with_space('Bright:', space_for_layout_online_bulbs_right),
                                   sg.Text("NO DATA", key='-TEXT-BRIGHT-STATUS-')],
+                                 [set_name_with_space('Color mode:', space_for_layout_online_bulbs_right),
+                                  sg.Text("NO DATA", key='-TEXT-COLOR-MODE-STATUS-')],
                                  [sg.Text("RGB Color:", size=(25, 1), justification='center',
                                           font=("Helvetica", 10), relief=sg.RELIEF_RIDGE, expand_x=True)],
                                  [sg.Text("NO DATA", key='-TEXT-RGB-COLOR-STATUS-'),
@@ -33,12 +36,28 @@ def make_window():
                                           font=("Helvetica", 10), relief=sg.RELIEF_RIDGE, expand_x=True)],
                                  [sg.Text("NO DATA", key='-TEXT-COLOR-TEMPERATURE-STATUS-'),
                                   sg.Canvas(background_color=sg.theme_button_color()[1], size=(40, 20), expand_x=True,
-                                            key='-CANVAS-KELVIN-COLOR-')
-                                  ]
+                                            key='-CANVAS-KELVIN-COLOR-')]
                                  ]
 
     layout_online_bulbs = [[sg.Column(layout_online_bulbs_left, p=0, expand_y=True, expand_x=True),
-                            sg.Column(layout_online_bulbs_right, p=0, expand_y=True, expand_x=True)]
+                            sg.Column(layout_online_bulbs_right, p=0, expand_y=True, expand_x=True)],
+                           [sg.Text("Set bulb's properties:", size=(25, 1), justification='center',
+                                    font=("Helvetica", 12), relief=sg.RELIEF_RIDGE, expand_x=True)],
+                           [set_name_with_space('Power: ', space_for_layout_online_bulbs_set_properties),
+                            sg.Button('NO DATA', expand_x=True, key='-BUTTON-SET-POWER-')],
+                           [set_name_with_space('Bright: ', space_for_layout_online_bulbs_set_properties),
+                            sg.Slider((1, 100), orientation='h', size=(10, 15), expand_x=True)],
+                           [set_name_with_space('Temp: ', space_for_layout_online_bulbs_set_properties),
+                            sg.Slider((1700, 6500), resolution=100, orientation='h', size=(10, 15), expand_x=True),
+                            sg.Canvas(background_color=sg.theme_button_color()[1], size=(50, 30),
+                                      key='-CANVAS-SET-KELVIN-COLOR-'),
+                            sg.Button('Set', size=(8, 1), key='-BUTTON-SET-TEMP-COLOR-')],
+                           [set_name_with_space('RGB: ', space_for_layout_online_bulbs_set_properties),
+                            sg.Text('', expand_x=True),
+                            sg.ColorChooserButton("Choose", border_width=1, size=(8, 1),
+                                                  key='-COLOR-CHOOSER-BUTTON-SET-RGB-COLOR-'),
+                            sg.Text("{'R': 0, 'G': 0, 'B': 0}", key='-TEXT-SET-RGB-COLOR-'),
+                            sg.Button('Set', size=(8, 1), key='-BUTTON-SET-RGB-COLOR-')]
                            ]
 
     layout = [[sg.Text('SMART BULBS CONTROLLER', size=(35, 1), justification='center', font=("Helvetica", 16),
@@ -79,10 +98,8 @@ def start_gui():
                                                             key_color_temperature_status=
                                                             '-TEXT-COLOR-TEMPERATURE-STATUS-',
                                                             key_canvas_rgb_color='-CANVAS-RGB-COLOR-',
-                                                            key_canvas_kelvin_color='-CANVAS-KELVIN-COLOR-')
-
-        if 'IF' in event:
-            pass
+                                                            key_canvas_kelvin_color='-CANVAS-KELVIN-COLOR-',
+                                                            key_color_mode_status='-TEXT-COLOR-MODE-STATUS-')
 
     window.close()
 
--- GUI/GUI_events.py
+++ GUI/GUI_events.py
@@ -8,14 +8,15 @@ def event_update_listbox_of_discovered_bulbs(window, key, bulbs_list=Engine.Bulb
 
 
 def event_update_bulb_properties(window, key_power_status, key_bright_status, key_rgb_color_status,
-                                 key_color_temperature_status, key_canvas_rgb_color, key_canvas_kelvin_color, bulb):
+                                 key_color_temperature_status, key_canvas_rgb_color, key_canvas_kelvin_color,
+                                 key_color_mode_status, bulb):
     bulb_properties = bulb.get_properties()
     window[key_power_status].update(bulb_properties['power'])
     window[key_bright_status].update(bulb_properties['bright'])
     window[key_rgb_color_status].update(Engine.Color_translation.get_rgb_from_decimal(bulb_properties['rgb']))
     window[key_color_temperature_status].update(bulb_properties['ct'])
-    window[key_canvas_rgb_color].\
+    window[key_canvas_rgb_color]. \
         update(background_color=Engine.Color_translation.get_hex_from_decimal(bulb_properties['rgb']))
     window[key_canvas_kelvin_color].update(background_color=Engine.Color_translation.get_hex_from_rgb(
         Engine.Kelvin_color.get_rgb_color_from_kelvin_temperature_color(int(bulb_properties['ct']))))
-
+    window[key_color_mode_status].update((lambda x: 'RGB' if x == 1 else 'TEMP')(int(bulb_properties['color_mode'])))

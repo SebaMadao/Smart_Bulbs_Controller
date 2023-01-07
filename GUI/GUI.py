1c893ce0715643b7509e770e407a5e331b7779de
Fronc <sebastian.fronc.code@gmail.com>
2023-01-07 22:01:49 +0100

    add working power button

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,6 +1,6 @@
 import PySimpleGUI as sg
 import GUI_events
-
+import yeelight
 
 def make_window():
     theme = 'DarkGrey15'
@@ -44,20 +44,22 @@ def make_window():
                            [sg.Text("Set bulb's properties:", size=(25, 1), justification='center',
                                     font=("Helvetica", 12), relief=sg.RELIEF_RIDGE, expand_x=True)],
                            [set_name_with_space('Power: ', space_for_layout_online_bulbs_set_properties),
-                            sg.Button('NO DATA', expand_x=True, key='-BUTTON-SET-POWER-')],
+                            sg.Button('POWER', expand_x=True, key='-BUTTON-SET-UP-POWER-')],
                            [set_name_with_space('Bright: ', space_for_layout_online_bulbs_set_properties),
-                            sg.Slider((1, 100), orientation='h', size=(10, 15), expand_x=True)],
+                            sg.Slider((1, 100), orientation='h', size=(10, 15), expand_x=True,
+                                      key='-BUTTON-SET-BRIGHT-'),
+                            sg.Button('Set', size=(8, 1), key='-BUTTON-SET-UP-BRIGHT-')],
                            [set_name_with_space('Temp: ', space_for_layout_online_bulbs_set_properties),
                             sg.Slider((1700, 6500), resolution=100, orientation='h', size=(10, 15), expand_x=True),
                             sg.Canvas(background_color=sg.theme_button_color()[1], size=(50, 30),
                                       key='-CANVAS-SET-KELVIN-COLOR-'),
-                            sg.Button('Set', size=(8, 1), key='-BUTTON-SET-TEMP-COLOR-')],
+                            sg.Button('Set', size=(8, 1), key='-BUTTON-SET-UP-TEMP-COLOR-')],
                            [set_name_with_space('RGB: ', space_for_layout_online_bulbs_set_properties),
                             sg.Text('', expand_x=True),
                             sg.ColorChooserButton("Choose", border_width=1, size=(8, 1),
                                                   key='-COLOR-CHOOSER-BUTTON-SET-RGB-COLOR-'),
                             sg.Text("{'R': 0, 'G': 0, 'B': 0}", key='-TEXT-SET-RGB-COLOR-'),
-                            sg.Button('Set', size=(8, 1), key='-BUTTON-SET-RGB-COLOR-')]
+                            sg.Button('Set', size=(8, 1), key='-BUTTON-SET-UP-RGB-COLOR-')]
                            ]
 
     layout = [[sg.Text('SMART BULBS CONTROLLER', size=(35, 1), justification='center', font=("Helvetica", 16),
@@ -72,6 +74,14 @@ def make_window():
     return window
 
 
+def check_if_it_bulb(key_selected_bulb, values):
+    if values[key_selected_bulb]:
+        if isinstance(values[key_selected_bulb][0], yeelight.Bulb):
+            return True
+        else:
+            return False
+
+
 def start_gui():
     window = make_window()
     while True:
@@ -80,26 +90,45 @@ def start_gui():
         if event == sg.WIN_CLOSED:
             break
 
-        if event:
-            pass
-
         if event == '-BUTTON-SEARCH-BULBS-':
             GUI_events.event_update_listbox_of_discovered_bulbs(window=window, key='-LISTBOX-SEARCH-BULBS-')
 
         if event == '-LISTBOX-SEARCH-BULBS-':
-            if values['-LISTBOX-SEARCH-BULBS-']:
-                if isinstance(values['-LISTBOX-SEARCH-BULBS-'][0], str):
-                    pass
-                else:
-                    GUI_events.event_update_bulb_properties(window=window, bulb=values['-LISTBOX-SEARCH-BULBS-'][0],
-                                                            key_power_status='-TEXT-POWER-STATUS-',
-                                                            key_bright_status='-TEXT-BRIGHT-STATUS-',
-                                                            key_rgb_color_status='-TEXT-RGB-COLOR-STATUS-',
-                                                            key_color_temperature_status=
-                                                            '-TEXT-COLOR-TEMPERATURE-STATUS-',
-                                                            key_canvas_rgb_color='-CANVAS-RGB-COLOR-',
-                                                            key_canvas_kelvin_color='-CANVAS-KELVIN-COLOR-',
-                                                            key_color_mode_status='-TEXT-COLOR-MODE-STATUS-')
+            if check_if_it_bulb(key_selected_bulb=event, values=values):
+                GUI_events.event_update_bulb_properties(window=window, bulb=values['-LISTBOX-SEARCH-BULBS-'][0],
+                                                        key_power_status='-TEXT-POWER-STATUS-',
+                                                        key_bright_status='-TEXT-BRIGHT-STATUS-',
+                                                        key_rgb_color_status='-TEXT-RGB-COLOR-STATUS-',
+                                                        key_color_temperature_status=
+                                                        '-TEXT-COLOR-TEMPERATURE-STATUS-',
+                                                        key_canvas_rgb_color='-CANVAS-RGB-COLOR-',
+                                                        key_canvas_kelvin_color='-CANVAS-KELVIN-COLOR-',
+                                                        key_color_mode_status='-TEXT-COLOR-MODE-STATUS-',
+                                                        key_button_set_power='-BUTTON-SET-UP-POWER-')
+            else:
+                pass
+
+        if event == '-BUTTON-SET-UP-POWER-':
+            if check_if_it_bulb(key_selected_bulb='-LISTBOX-SEARCH-BULBS-', values=values):
+                GUI_events.event_set_up_power(bulb=values['-LISTBOX-SEARCH-BULBS-'][0])
+
+        if event == '-BUTTON-SET-UP-BRIGHT-':
+            if check_if_it_bulb(key_selected_bulb='-LISTBOX-SEARCH-BULBS-', values=values):
+                GUI_events.event_set_up_bright(bulb=values['-LISTBOX-SEARCH-BULBS-'][0],
+                                               bright=values['-BUTTON-SET-BRIGHT-'])
+
+        if '-SET-UP-' in event:
+            if check_if_it_bulb(key_selected_bulb='-LISTBOX-SEARCH-BULBS-', values=values):
+                GUI_events.event_update_bulb_properties(window=window, bulb=values['-LISTBOX-SEARCH-BULBS-'][0],
+                                                        key_power_status='-TEXT-POWER-STATUS-',
+                                                        key_bright_status='-TEXT-BRIGHT-STATUS-',
+                                                        key_rgb_color_status='-TEXT-RGB-COLOR-STATUS-',
+                                                        key_color_temperature_status=
+                                                        '-TEXT-COLOR-TEMPERATURE-STATUS-',
+                                                        key_canvas_rgb_color='-CANVAS-RGB-COLOR-',
+                                                        key_canvas_kelvin_color='-CANVAS-KELVIN-COLOR-',
+                                                        key_color_mode_status='-TEXT-COLOR-MODE-STATUS-',
+                                                        key_button_set_power='-BUTTON-SET-UP-POWER-')
 
     window.close()
 
--- GUI/GUI_events.py
+++ GUI/GUI_events.py
@@ -1,6 +1,7 @@
 import Engine.Bulbs_search
 import Engine.Color_translation
 import Engine.Kelvin_color
+import yeelight
 
 
 def event_update_listbox_of_discovered_bulbs(window, key, bulbs_list=Engine.Bulbs_search.get_list_of_bulbs()):
@@ -9,7 +10,7 @@ def event_update_listbox_of_discovered_bulbs(window, key, bulbs_list=Engine.Bulb
 
 def event_update_bulb_properties(window, key_power_status, key_bright_status, key_rgb_color_status,
                                  key_color_temperature_status, key_canvas_rgb_color, key_canvas_kelvin_color,
-                                 key_color_mode_status, bulb):
+                                 key_color_mode_status, key_button_set_power, bulb):
     bulb_properties = bulb.get_properties()
     window[key_power_status].update(bulb_properties['power'])
     window[key_bright_status].update(bulb_properties['bright'])
@@ -20,3 +21,32 @@ def event_update_bulb_properties(window, key_power_status, key_bright_status, ke
     window[key_canvas_kelvin_color].update(background_color=Engine.Color_translation.get_hex_from_rgb(
         Engine.Kelvin_color.get_rgb_color_from_kelvin_temperature_color(int(bulb_properties['ct']))))
     window[key_color_mode_status].update((lambda x: 'RGB' if x == 1 else 'TEMP')(int(bulb_properties['color_mode'])))
+
+
+def event_set_up_power(bulb):
+    bulb_properties = bulb.get_properties()
+    if bulb_properties['power'] == 'on':
+        bulb.turn_off()
+    else:
+        bulb.turn_on()
+
+
+def event_update_color_chooser_button(key_button):
+    pass
+
+
+def event_update_canvas_kelvin_color(key_canvas):
+    pass
+
+
+def event_set_up_bright(bulb, bright):
+    bulb.set_brightness(int(bright))
+
+
+def event_set_up_rgb_color(bulb):
+    pass
+
+
+def event_set_up_kelvin_color(bulb):
+    pass
+
